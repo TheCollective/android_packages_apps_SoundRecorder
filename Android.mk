7c06d83f8428c04d6c184974f52cbcfeb01d4e97
Roger Michaud <cptjohn47@gmail.com>
2013-07-17 12:31:31 -0500

    Change to MIUI soundrecorder

--- Android.mk
+++ Android.mk
@@ -1,10 +1,11 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_SRC_FILES := $(call all-subdir-java-files) $(call all-renderscript-files-under, src)
 
 LOCAL_PACKAGE_NAME := SoundRecorder
+LOCAL_CERTIFICATE := shared
 
 include $(BUILD_PACKAGE)
